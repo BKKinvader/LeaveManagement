80065e75ad14ba56faea7b503962c6401034affe
Dennis Ekner <114058184+Deekner@users.noreply.github.com>
2023-10-10 14:52:02 +0200

    Added Employee Service and interface for it as well as endpoints

---
+++ LeaveManagement_API/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using LeaveManagement_API.Model;
+
+namespace LeaveManagement_API.Services
+{
+    public interface IEmployeeService
+    {
+        public Task<IEnumerable<Employee>> GetAllEmployees();
+        public Task<Employee> GetEmployeeById(Guid id);
+        public Task<Employee> AddEmployee(Employee employee);
+        public Task<Employee> UpdateEmployee(Employee employee);
+        public Task<Employee> DeleteEmployee(Guid id);
+    }
+}
