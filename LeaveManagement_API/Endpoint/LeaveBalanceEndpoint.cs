01da69ffc0f94d02cf545e43db4f224574c553ac
Tim Nilsson <60481888+BKKinvader@users.noreply.github.com>
2023-10-11 13:15:02 +0200

    LeaveBalanceDTO

--- LeaveManagement_API/Endpoint/LeaveBalanceEndpoint.cs
+++ LeaveManagement_API/Endpoint/LeaveBalanceEndpoint.cs
@@ -13,8 +13,8 @@ namespace LeaveManagement_API.Endpoint
         {
             app.MapGet("/api/LeaveBalance/", GetAllLeaveBalance).WithName("GetLeaveBalance").Produces<APIResponse>(200);
             app.MapGet("/api/LeaveBalance/{id:int}", GetLeaveBalanceById).WithName("GetLeaveBalanceById").Produces<APIResponse>(200);
-            app.MapPost("/api/LeaveBalance", CreateLeaveBalance).WithName("CreateLeaveBalance").Accepts<LeaveRequest>("application/json").Produces<APIResponse>(201).Produces(400);
-            app.MapPut("/api/LeaveBalance/{id:int}", UpdateLeaveBalance).WithName("UpdateELeaveBalance").Accepts<LeaveRequest>("application/json").Produces<APIResponse>(200).Produces(400);
+            app.MapPost("/api/LeaveBalance", CreateLeaveBalance).WithName("CreateLeaveBalance").Accepts<LeaveBalanceDTO>("application/json").Produces<APIResponse>(201).Produces(400);
+            app.MapPut("/api/LeaveBalance/{id:int}", UpdateLeaveBalance).WithName("UpdateELeaveBalance").Accepts<LeaveBalanceDTO>("application/json").Produces<APIResponse>(200).Produces(400);
             app.MapDelete("/api/LeaveBalance/{id:int}", DeleteLeaveBalanceById).WithName("DeleteLeaveBalance").Produces<APIResponse>(200);
         }
 
