9c9b7034079b880011cf8f8d5ad45896fc8de347
Dennis Ekner <114058184+Deekner@users.noreply.github.com>
2023-10-11 10:51:15 +0200

    Added LeaveRequestService as well as Interface for it and endpoints. Also injected to container

--- LeaveManagement_API/Model/LeaveRequest.cs
+++ LeaveManagement_API/Model/LeaveRequest.cs
@@ -5,11 +5,17 @@ namespace LeaveManagement_API.Model
     public class LeaveRequest
     {
         public Guid Id { get; set; }
+        [Required]
         public Guid EmployeeId { get; set; }
+        [Required]
         public int LeaveTypeId { get; set; }
+        [Required]
         public DateTime StartDate { get; set; }
+        [Required]
         public DateTime EndDate { get; set; }
+        [Required]
         public LeaveStatus Status { get; set; }
+        [MaxLength(255)]
         public string Comments { get; set; }
 
         public Employee Employee { get; set; }
