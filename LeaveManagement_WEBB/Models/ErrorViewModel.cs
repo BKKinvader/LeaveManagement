a6635976fb98dafe176494f798cfea66e8c92932
Dennis Ekner <114058184+Deekner@users.noreply.github.com>
2023-10-16 13:32:00 +0200

    Recreation

---
+++ LeaveManagement_WEBB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagement_WEBB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
